902ce5f70e9aa08080b8a9555e0e0b19858324bf
chev <chevv0004@gmail.com>
2025-10-10 21:17:17 -0500

    feat: mock avatar url constant

--- src/components/ui/Inbox/NotificationEntry.stories.tsx
+++ src/components/ui/Inbox/NotificationEntry.stories.tsx
@@ -1,3 +1,4 @@
+import { MOCK_USER_AVATAR_URL } from '@/constants/mock-data';
 import type { Meta, StoryObj } from '@storybook/react';
 import { useTranslations } from 'next-intl';
 import { NotificationEntry } from './NotificationEntry';
@@ -31,15 +32,14 @@ type Story = StoryObj<typeof NotificationEntry>;
 
 export const Default: Story = {
   render: () => {
-    const t = useTranslations();
+    const t = useTranslations('Inbox');
     return (
       <NotificationEntry
         notification={{
           id: '1',
           message: t('anonymousUserCopied'),
           timestamp: t('minutesAgo', { count: 2 }),
-          iconUrl:
-            'https://cdn.discordapp.com/avatars/559278744330698752/05acb5001d40db956558f9cfdbe6414d.webp?size=1024',
+          iconUrl: MOCK_USER_AVATAR_URL,
           read: false,
         }}
         onMarkAsRead={() => {}}
