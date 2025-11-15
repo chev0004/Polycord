999faa3ee6052f519981529fe058d614e957fc8f
Chev <chevv0004@gmail.com>
2025-11-15 00:32:43 -0600

    chore: update mock user avatar URL

--- src/constants/mock-data.ts
+++ src/constants/mock-data.ts
@@ -1,2 +1,2 @@
 export const MOCK_USER_AVATAR_URL =
-  'https://cdn.discordapp.com/avatars/559278744330698752/05acb5001d40db956558f9cfdbe6414d.webp?size=1024';
+  'https://cdn.discordapp.com/avatars/559278744330698752/07843b224a1b639f1444f16307e9a493.webp?size=1024';
