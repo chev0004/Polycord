045f542e1d0f57dcdef9eaf2750828c7b5f79299
chev <chevv0004@gmail.com>
2026-06-12 20:27:18 -0500

    test: add language switcher and user menu stories

---
+++ src/features/Navbar/LanguageSwitcher.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, screen, userEvent, within } from '@storybook/test';
+import { LanguageSwitcher } from './LanguageSwitcher';
+
+const meta: Meta<typeof LanguageSwitcher> = {
+  title: 'Components/LanguageSwitcher',
+  component: LanguageSwitcher,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: '/en',
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex justify-end bg-background-darker p-10">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof LanguageSwitcher>;
+
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const trigger = await canvas.findByRole('button', {
+      name: 'Change language',
+    });
+
+    await userEvent.click(trigger);
+
+    await expect(await screen.findByText('English')).toBeInTheDocument();
+    await expect(await screen.findByText('Japanese')).toBeInTheDocument();
+  },
+};
