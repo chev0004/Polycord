857f8382a8838fef60e9659f2ebe172732a859bf
chev <chevv0004@gmail.com>
2026-06-22 15:47:40 -0500

    feat: hide blocked users from discovery feed

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import type { AvailabilityPattern } from '@/constants/availability';
 import {
   getProfileByUserId,
+  listBlockedUserIds,
   listSavedProfileIds,
   toViewerAvailabilityContext,
   upsertDiscordUser,
@@ -24,6 +25,7 @@ export default async function Home({
   const user = await getCurrentUser();
   let needsOnboarding = false;
   let savedProfileIds: string[] = [];
+  let blockedUserIds: string[] = [];
   let currentProfileId: string | undefined;
   let bumpReadyAt: string | undefined;
   let viewerTimezone: string | undefined;
@@ -35,6 +37,7 @@ export default async function Home({
     needsOnboarding = !profile;
     currentProfileId = profile?.profile.id;
     savedProfileIds = await listSavedProfileIds(persistedUser.id);
+    blockedUserIds = await listBlockedUserIds(persistedUser.id);
 
     if (profile) {
       const viewer = toViewerAvailabilityContext(profile.profile);
@@ -76,6 +79,7 @@ export default async function Home({
         locale={lang}
         needsOnboarding={needsOnboarding}
         savedProfileIds={savedProfileIds}
+        blockedUserIds={blockedUserIds}
         currentProfileId={currentProfileId}
         bumpReadyAt={bumpReadyAt}
         viewerTimezone={viewerTimezone}
